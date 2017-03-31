88591a9c3de7921a88468b8f5cbf7e9bbd3ad520
ovicornea <ovi.cornea@gmail.com>
2017-03-31 08:18:06 +0200

    first run

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Ovi Cornea on 3/31/17.
+//  Copyright © 2017 Ovi Cornea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
