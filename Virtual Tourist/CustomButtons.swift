d79036040383851b898e30dc53ee2012019b5bb9
ovicornea <ovi.cornea@gmail.com>
2017-03-31 08:47:36 +0200

    storyboard completed

---
+++ Virtual Tourist/CustomButtons.swift
@@ -0,0 +1,21 @@
+//
+//  CustomButtons.swift
+//  Virtual Tourist
+//
+//  Created by Ovi Cornea on 3/31/17.
+//  Copyright © 2017 Ovi Cornea. All rights reserved.
+//
+
+import UIKit
+
+class CustomButtons: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Virtual Tourist/MapVC.swift
+++ Virtual Tourist/MapVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapVC.swift
 //  Virtual Tourist
 //
 //  Created by Ovi Cornea on 3/31/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
